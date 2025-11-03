6e563700bc9d65600eb666ed48a9f3effdc66a9c
Delon-Wu <2474323762@qq.com>
2025-11-04 01:18:02 +0800

    Init

---
+++ contracts/hello.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9 <0.9.0;
+
+contract Hello3Dot0 {
+    string public hello = "Hello 3.0";
+
+    // learning data type
+    // int public account = 1 * 2 ** 255 - 1;
+    // uint public account1 = 1 * 2 ** 256 - 1;
+    // bool public flag = true;
+
+    // address public addr = 0x233FBb242353720Fa98115db73448a662A2D0888; // 16 20位 keccack256
+    // bytes32 public b1 = hex"1111";
+    // enum Status {
+    //     Active,
+    //     Inactive
+    // }
+    // Status public status = Status.Active;
+
+    // int[] public arr;
+    // string[] public arr2;
+    // bool[] public arr3;
+    // address public arr4;
+    // bytes10[] public arr5;
+    // string public str = "hello";
+    // struct Person {
+    //     string name;
+    //     bool isMan;
+    //     uint8 age;
+    // }
+    // Person public tom = Person("Tom", true, 19);
+
+    string private helloPrivate = "Hello, ";
+    function greeting1(string memory name) public view returns (string memory) {
+        return greeting2(name);
+    }
+
+    function changeHello(string memory newHello) public {
+        hello = newHello;
+    }
+
+    function greeting2(string memory name) internal view returns (string memory) {
+        return string.concat(helloPrivate, name);
+    }
+
+    function greeting3() public pure returns(string memory) {
+        return "Hello world!";
+    }
+
+    function learnFunction(
+        string memory name
+    ) public pure returns (string memory) {
+        // 1. 可见修饰符:
+        // public external private internal
+
+        // 2. 状态可变性修饰符
+        // view只读; pure不可读不可写; default
+        return string.concat("hello ", name);
+    }
+
+
+}
