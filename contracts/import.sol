8a87b562ab6ade4135fa3d86aefe5ebe53d5026c
Delon-Wu <2474323762@qq.com>
2025-11-13 01:48:34 +0800

    note: 学习了怎么导入合约，和合约的继承，方法重写，合约的部署

--- contracts/import.sol
+++ contracts/import.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8;
 
 import "contracts/array.sol"; // 本地导入
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // 远程导入
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol"; // 远程导入
 
 import {LearningLoop as Demo1} from "contracts/loop.sol"; // 导入时重命名
 // import "./task/task1/*"; // ???
@@ -14,18 +15,21 @@ contract MyToken is ERC20 {
 }
 
 contract Parent {
-    function foo() public pure virtual returns (string memory) {
+    function foo() public pure virtual returns (string memory) { // 希望被子合约覆盖就使用virtual
         return "Parent";
+    }
+    function getAddress() public view returns (address) {
+        return address(this); // 这里的this指向继承者（子合约）
     }
 }
 
 contract Child is Parent {
-    function foo() public pure virtual override returns (string memory) {
+    function foo() public pure override returns (string memory) {
         return "Child";
     }
 }
 
-contract Ownerable {
+contract Ownable {
     address public owner;
     constructor() {
         owner = msg.sender;
@@ -36,13 +40,12 @@ contract Ownerable {
     }
 }
 
-contract MyContract is ERC20, Ownerable {
-
-    constructor() ERC20("MyContract", "MC") {
-        _mint(msg.sender, 10000 * 10 ** decimals());
+contract MyContract is ERC20, ERC20Permit, Ownable { // 继承有限度取决于这里is后的顺序
+    constructor() ERC20("MyContract", "MC") ERC20Permit("MyContract") {
+        _mint(msg.sender, 1000000);
     }
 
-    function test(address newOwner) public onlyOwner {
-        owner = newOwner;
-    }
+    // function test(address newOwner) public onlyOwner {
+    //     owner = newOwner;
+    // }
 }
